17dc5b4e2afda51c4765d38788ea55fab855a388
Ikkew-san <ekkarat.no@gmail.com>
2018-04-20 13:48:59 +0700

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,8 @@ $app->withFacades();
 
 $app->withEloquent();
 
+$app->configure('cors');
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
@@ -59,13 +61,14 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
+$app->middleware([
 //    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+    \Barryvdh\Cors\HandleCors::class,
+]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Authenticate::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
@@ -78,7 +81,8 @@ $app->singleton(
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(Barryvdh\Cors\ServiceProvider::class);
+//$app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
